31e9dc857b42169ee147e926638a1216f29b92a7
JunaidMovido <jh@movido-media.com>
2023-06-12 15:19:30 +0200

    updated intro page

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Junaid Hossein";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Technical Product Manager, Tech Consultant";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const MY_NAME = "Junaid";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
