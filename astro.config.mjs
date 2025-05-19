6cb689024a81075ee262ef36062bb74559b1bfa4
Junaid101 <junaid.h.gazi@gmail.com>
2025-05-19 18:59:38 +0200

    updated tag page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
+  output : 'static',
   site: 'https://www.junaidhossein.com',
 });
